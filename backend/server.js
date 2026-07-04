780d9132f16ebda577c7091dadbfac87ac43e3fd
BHOOMI KESHARWANI <bhoomikesharwani070@gmail.com>
2026-07-04 20:05:07 +0530

    Fix start script

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ mongoose
 /* =======================
    SERVER
 ======================= */
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT} 🚀`);
 });
